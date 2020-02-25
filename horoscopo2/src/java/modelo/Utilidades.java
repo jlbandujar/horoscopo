28725a1978bc55104cc88ec89162eda49b919e76
Jose Luis <jlbandujar@gmail.com>
2020-02-25 01:05:16 +0100

    Versión 1.0 Carga la lista de dias y de meses

---
+++ horoscopo2/src/java/modelo/Utilidades.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jose
+ */
+public class Utilidades {
+    public static ArrayList<String> getMeses(){
+        ArrayList<String> meses = new ArrayList<String>();
+        for(int i=1;i<=12;i++){
+            meses.add(String.valueOf(i));
+        }
+        return meses;
+    }
+    
+    public static ArrayList<String> getDias(){
+        ArrayList<String> dias = new ArrayList<String>();
+        for(int i=1;i<=31;i++){
+            dias.add(String.valueOf(i));
+        }
+        return dias;
+    }
+    
+    public static String getHoroscopo( int dia, int mes ){
+        String signo="";
+        switch ( mes ){
+            case 1: 
+                if ( dia>20 ) {
+                    signo="Acuario";
+                } else {
+                    signo="Capricornio";
+                }
+                break;
+            case 2: 
+                if ( dia>20 ) {
+                    signo="Piscis";
+                } else {
+                    signo="Acuario";
+                }
+                break;
+            case 3: 
+                if ( dia>20 ) {
+                    signo="Aries";
+                } else {
+                    signo="Piscis";
+                }
+                break;
+        }
+        return signo;
+    }
+}
+            
+  
---
+++ horoscopo2/web/horoscopo.jsp
@@ -0,0 +1,41 @@
+<%-- 
+    Document   : horoscopo
+    Created on : 24-feb-2020, 22:01:18
+    Author     : Jose
+--%>
+
+<%@page import="java.util.ArrayList"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Horóscopo</h1>
+        <form action="horoscopo">
+            <label for="nombre">Nombre:</label><input type="text" 
+                                                      id="nombre" name="nombre"/>
+            <% ArrayList<String> meses = (ArrayList<String>) 
+                    request.getAttribute("meses"); %>
+            
+            <br><label for="mes">Mes:</label>
+            <select name="mes" id="mes">
+            <% for( int i=0; i<meses.size(); i++){ %>
+            <option value="<%=meses.get(i)%>"><%=meses.get(i)%> </option>
+            <% } %>
+
+            </select>
+            <% ArrayList<String> dias = (ArrayList<String>) 
+                    request.getAttribute("dias"); %>
+            
+            <br><label for="dia">Dia:</label><select name="dia" id="dia">
+            <% for( int d=0; d<dias.size(); d++){ %>
+                <option value="<%=dias.get(d)%>"><%=dias.get(d)%> </option>   
+             <% } %>
+            </select>
+            <input type="submit" value="Enviar">
+        </form>
+    </body>
+</html>
